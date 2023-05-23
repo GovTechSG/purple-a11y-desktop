6abdd85f4f4220f9f277ef306a2d73ab911aa3a8
greyguy21 <e0559779@u.nus.edu>
2023-05-23 14:58:05 +0800

    Update playwright.yml

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    screenshot: 'on', 
+    screenshot: 'only-on-failure', 
   },
 
   /* Configure projects for major browsers */
