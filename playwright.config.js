a027942699bf62c94943581397e0fe73cbf439f9
greyguy21 <e0559779@u.nus.edu>
2023-05-24 10:39:35 +0800

    Increase test timeout

--- playwright.config.js
+++ playwright.config.js
@@ -23,6 +23,7 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  timeout: 300000,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
