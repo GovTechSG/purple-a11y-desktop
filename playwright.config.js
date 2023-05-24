21aa48fb542f68e5d8c62d49fa3062039ef68d58
greyguy21 <e0559779@u.nus.edu>
2023-05-24 09:55:13 +0800

    Set global timeout

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,7 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  timeout: 10000 * 30, 
+  globalTimeout: 10000 * 18, 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
