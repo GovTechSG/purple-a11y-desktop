899f888eb6c1b2a3efa39c030573bac2d96a8420
greyguy21 <e0559779@u.nus.edu>
2023-06-26 15:12:00 +0800

    Add accessibility features to dropdown

--- src/common/components/DownloadFolderDropdown.jsx
+++ src/common/components/DownloadFolderDropdown.jsx
@@ -7,27 +7,95 @@ import { useEffect, useState } from "react";
 import services from "../../services";
 
 const DownloadFolderDropdown = () => {
+    const dropdownOptionActiveClass = "download-dropdown-btn dropdown-list-btn active";
+    const dropdownOptionInactiveClass = "download-dropdown-btn dropdown-list-btn";
+
+    const [active, setActive] = useState(true); 
     const [exportDir, setExportDir] = useState();
     const [openDropDown, setOpenDropDown] = useState(false);
-   
+     
     useEffect(() => {
         const getExportDir = async () => {
             const userData = await services.getUserData();
             setExportDir(userData.exportDir);
         };
-
         getExportDir();
     }, [])
 
+    useEffect(() => {
+        const handleKeyDown = (event) =>  {
+            const dropdownListElement = document.querySelector('.download-dropdown-list'); 
+            const dropdownOptions = dropdownListElement.querySelectorAll('.dropdown-list-btn'); 
+
+            if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+                console.log(event.key);
+                if (!dropdownListElement.contains(event.target)) {
+                    event.preventDefault();
+                    dropdownOptions[0].focus();
+                }
+
+                if (event.target === dropdownOptions[0]) {
+                    event.preventDefault(); 
+                    dropdownOptions[1].focus();
+                }
+
+                if (event.target === dropdownOptions[1]) {
+                    event.preventDefault(); 
+                    dropdownOptions[0].focus();
+                }
+            }
+
+            if (event.key === 'Tab') {
+                event.preventDefault();
+                const dropdownButton = document.querySelector('.dir-info-btn'); 
+                dropdownButton.focus();
+                setOpenDropDown(false);
+            }
+
+            if (event.key === 'Escape') {
+                event.preventDefault();
+                const dropdownButton = document.querySelector('.dir-info-btn'); 
+                dropdownButton.focus();
+                setOpenDropDown(false);
+            }
+        }
+        if (openDropDown) {
+            document.addEventListener('keydown', handleKeyDown); 
+        }
+    }, [openDropDown])
+
+    const handleKeydownOpenDropdown = (event) => {
+        if (event.key === 'ArrowDown') {
+            event.preventDefault();
+            setOpenDropDown(true); 
+        }
+
+        if (event.key === 'ArrowUp') {
+            event.preventDefault();
+            setOpenDropDown(false); 
+        }
+
+        if (event.key === 'Enter') {
+            event.preventDefault();
+        }
+    }
+
+
+    const handleClickCurrent = () => {
+        setOpenDropDown(false);
+    }
+
     const handleSetExportDir = async () => {
+        setActive(false)
         const exportDir = await window.services.setExportDir(); 
         setExportDir(exportDir);
         setOpenDropDown(false);
+        setActive(true)
     }
 
     return (
         <div className="download-dropdown fade-in">
-        <Button className="dir-info-button download-dropdown-btn" onClick={() => {setOpenDropDown(!openDropDown)}}>
+        <button aria-haspopup="menu" className="dir-info-btn download-dropdown-btn" onClick={() => {setOpenDropDown(!openDropDown)}} onKeyDown={(e) => handleKeydownOpenDropdown(e)}>
           <img id="folder-img" src={folder}></img>
           <span id="dir-info">{exportDir}</span>
           {openDropDown ? (
@@ -41,10 +109,17 @@ const DownloadFolderDropdown = () => {
               svgIcon={<ChevronDownIcon />}
             />
           )}
-        </Button>
-        {openDropDown && <Button className="download-dropdown-btn choose-dir-button" onClick={handleSetExportDir}>Choose Another</Button>}
+        </button>
+        {openDropDown && 
+            <div className="download-dropdown-list" role="menu">
+                <button className={active ? dropdownOptionActiveClass : dropdownOptionInactiveClass} onClick={handleClickCurrent} role="menuitem" aria-selected="true">
+                    <span id="dir-info">{exportDir}</span>
+                </button>
+                <button className={!active ? dropdownOptionActiveClass : dropdownOptionInactiveClass} role="menuitem" onClick={handleSetExportDir}>Choose Another</button>
+            </div>
+        }
       </div>
-    )
-}
+    )                                     
+}           
 
 export default DownloadFolderDropdown;
