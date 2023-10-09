0547e20f3d3b7ec6321cbeed880e4cc2fc6816e7
jodichoo <jodichoo@users.noreply.github.com>
2023-10-09 11:45:52 +0800

    Add prepare step screen for custom flow

--- src/MainWindow/CustomFlow/index.jsx
+++ src/MainWindow/CustomFlow/index.jsx
@@ -2,7 +2,11 @@ import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
 import { cliErrorCodes, cliErrorTypes } from "../../common/constants";
 import services from "../../services";
-import './CustomFlow.scss'; 
+import './CustomFlow.scss';
+import arrowRight from "../../assets/arrow-right.svg";
+import editIcon from "../../assets/box-edit.svg";
+import exclaimTriangleIcon from "../../assets/exclamation-triangle.svg";
+import thumbsUpIcon from "../../assets/hand-thumbs-up.svg";
 import recordIcon from "../../assets/record-icon.svg";
 import replayIcon from "../../assets/replay-icon.svg"; 
 import labelIcon from "../../assets/label-icon.svg";
@@ -12,6 +16,90 @@ import CustomFlowDisplay from "./components/CustomFlowDisplay";
 import ButtonWithBoxRightArrowIcon from "./components/ButtonWithBoxRightArrowIcon";
 import DoneScanningStatus from "../../common/components/DoneScanningStatus";
 import LoadingScanningStatus from "../../common/components/LoadingScanningStatus";
+import Alert from "../../common/components/Alert";
+
+const PrepareStep = ({ scanDetails, setStep }) => {
+  const [isPostLogin, setIsPostLogin] = useState(null);
+  const [isFileUpload, setIsFileUpload] = useState(null);
+  const [fileUploadFolder, setFileUploadFolder] = useState(undefined);
+
+  useEffect(() => {
+    const getUploadFolderPath = async () => {
+      const path = await services.getUploadFolderPath();
+      setFileUploadFolder(path);
+    };
+    getUploadFolderPath();
+  }, []);
+
+  const handleOpenUploadFolder = async (e) => {
+    e.preventDefault();
+    window.services.openResultsFolder(fileUploadFolder);
+  };
+
+  const onClickNextStep = (e) => {
+    e.preventDefault();
+    setStep(2);
+  };
+
+  const handleCheckInput = (setState, stateVal) => {
+    return (e) => {
+      e.target.checked = true;
+      setState(stateVal);
+    };
+  };
+
+  const isAllSet = isPostLogin !== null && isFileUpload !== null && !isPostLogin && !isFileUpload;
+
+  return (
+    <>
+      <CustomFlowDisplay
+        icon={editIcon}
+        step={1}
+        title="Prepare"
+        url={scanDetails.scanUrl}
+      />
+      <form onSubmit={onClickNextStep}>
+        <div className="prepare-form-section">
+          <p className="bold-text">Does your custom flow have any post-login webpages?</p>
+          <label className="prepare-input-field">
+            <input className="form-check-input" name="post-login" id="post-login-yes-input" type="radio" value={isPostLogin} onChange={handleCheckInput(setIsPostLogin, true)} /> Yes
+          </label>
+          <label className="prepare-input-field">
+            <input className="form-check-input" name="post-login" id="post-login-no-input" type="radio" value={!isPostLogin} onChange={handleCheckInput(setIsPostLogin, false)} /> No
+          </label>
+          {isPostLogin &&
+          <Alert alertClassName="alert-custom mb-5 mt-3" icon={exclaimTriangleIcon}>
+            Ensure post-login webpages can be revisited using the same login credentials to allow Purple HATS to replay the steps for scanning.
+          </Alert>}
+        </div>
+        
+        <div className="prepare-form-section">
+          <p className="bold-text">Does your custom flow have any file upload fields?</p>
+          <label className="prepare-input-field">
+            <input className="form-check-input" name="file-upload" id="post-login-yes-input" type="radio" value={isFileUpload} onChange={handleCheckInput(setIsFileUpload, true)} /> Yes
+          </label>
+          <label className="prepare-input-field">
+            <input className="form-check-input" name="file-upload" id="post-login-no-input" type="radio" value={!isFileUpload} onChange={handleCheckInput(setIsFileUpload, false)} /> No
+          </label>
+          {isFileUpload && fileUploadFolder && 
+          <Alert alertClassName="alert-custom mb-5 mt-3" icon={exclaimTriangleIcon}>
+            If form contains file upload fields, place the same file{"(s)"} in{" "}
+            <a href="#" onClick={handleOpenUploadFolder}>{fileUploadFolder}</a>{" "}
+            to allow Purple HATS to attach the same file{"(s)"} when replaying the steps.
+          </Alert>}
+        </div>
+        {isAllSet && 
+        <Alert alertClassName="alert-custom mb-5" icon={thumbsUpIcon}>
+          It looks like you're all set for the custom flow scan, let's get started!
+        </Alert>}
+        <button className="primary custom-flow-button ms-auto" type="submit" disabled={isPostLogin === null && isFileUpload === null}>
+          Next step&nbsp;
+          <img src={arrowRight}></img>
+        </button>
+      </form>
+    </>
+  )
+};
 
 const CustomFlowPage = ({ completedScanId, setCompletedScanId }) => {
     const { state }= useLocation(); 
@@ -144,12 +232,6 @@ const CustomFlowPage = ({ completedScanId, setCompletedScanId }) => {
       setCustomFlowLabel(e.target.value);
     }
 
-    const handleOpenUploadFolder = async (e) => {
-      e.preventDefault();
-      const resultsPath = await services.getUploadFolderPath();
-      window.services.openResultsFolder(resultsPath);
-    }
-
     const generateReport = (e) => {
       e.preventDefault();
 
@@ -167,18 +249,20 @@ const CustomFlowPage = ({ completedScanId, setCompletedScanId }) => {
         switch (step) {
           case 1: {
             return (
+              <PrepareStep scanDetails={scanDetails} setStep={setStep} />
+            )
+          }
+          case 2: {
+            return (
               <>
                 <CustomFlowDisplay 
                   icon={recordIcon}
-                  step={1}
+                  step={2}
                   title={"Record"}
                   url={scanDetails.scanUrl}
                   description={
                     <>
                       Record your custom flow by manually navigating on a new browser window that will open automatically after starting the recording. In the event of a login page, we will solely capture your credentials for this scan and promptly remove them thereafter.
-                      <br></br><br></br>
-                      If you have any files to be uploaded during the scan, please drop them into{" "}
-                      <a href="#" onClick={handleOpenUploadFolder}>this folder</a>. Do not create any subfolders within the folder.
                       <br></br><br></br>
                       After finishing your flow, please close the browser to continue to the next step.
                     </>}
@@ -192,12 +276,12 @@ const CustomFlowPage = ({ completedScanId, setCompletedScanId }) => {
               </>
             )
           }
-          case 2: {
+          case 3: {
             return (
               <>
                 <CustomFlowDisplay
                   icon={replayIcon}
-                  step={2}
+                  step={3}
                   title={"Replay"}
                   url={scanDetails.scanUrl}
                   description={"Purple HATS will replay and scan the recorded flow on a new browser window."}
@@ -211,12 +295,12 @@ const CustomFlowPage = ({ completedScanId, setCompletedScanId }) => {
               </>
             )
           }
-          case 3: {
+          case 4: {
             return (
               <>
                 <CustomFlowDisplay
                   icon={labelIcon}
-                  step={3}
+                  step={4}
                   title={"Label"}
                   url={scanDetails.scanUrl}
                   description={"Assign a recognisable label to this custom flow for convenient reference in the report."}
