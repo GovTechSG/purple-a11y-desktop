7d297aa31353069851d99b99a68b904be688cbce
younglim <younglim@users.noreply.github.com>
2023-10-27 17:26:08 +0800

    Update forge.config.js for OSX signing and notarise

--- forge.config.js
+++ forge.config.js
@@ -6,6 +6,13 @@ module.exports = {
     osxUniversal: { // config options for `@electron/universal`
       x64ArchFiles: "*" // replace with any relevant glob pattern
     },
+    osxSign: {}, // object must exist even if empty
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID
+    },
     ignore: [
       'nodejs-mac-arm64',
       'nodejs-mac-x64',
