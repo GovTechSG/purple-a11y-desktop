97aec9cdab75879b8388d3d0659b42adfbcd0ee7
Thumster <bryan.thum@hotmail.com>
2023-11-01 16:20:43 +0800

    add useEffect

--- src/MainWindow/HomePage/AdvancedScanOptions.jsx
+++ src/MainWindow/HomePage/AdvancedScanOptions.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import Button from "../../common/components/Button";
 import SelectField from "./SelectField";
 import DownloadFolderDropdown from "../../common/components/DownloadFolderDropdown";
