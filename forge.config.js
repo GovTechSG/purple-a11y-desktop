65eb0330f843fbeed37b8ad6ee7d086b2bf27e94
jodichoo <jodi.choo@gmail.com>
2023-11-06 17:55:03 +0800

    Remove comments

--- forge.config.js
+++ forge.config.js
@@ -18,16 +18,6 @@ module.exports = {
         teamId: process.env.APPLE_TEAM_ID
       }
     }),
-    // osxSign: {
-    //   hardenedRuntime: true,
-    //   'gatekeeper-assess': false,
-    // }, 
-    // osxNotarize: {
-    //   tool: 'notarytool',
-    //   appleId: process.env.APPLE_ID,
-    //   appleIdPassword: process.env.APPLE_PASSWORD,
-    //   teamId: process.env.APPLE_TEAM_ID
-    // },
     ignore: [
       'nodejs-mac-arm64',
       'nodejs-mac-x64',
