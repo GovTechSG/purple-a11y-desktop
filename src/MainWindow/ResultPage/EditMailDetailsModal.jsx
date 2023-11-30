65380013eea4a802bb2e95c049eec2ef058c340e
jodichoo <jodi.choo@gmail.com>
2023-11-30 10:35:15 +0800

    Fix spacing, button classname for edit mail details modal

--- src/MainWindow/ResultPage/EditMailDetailsModal.jsx
+++ src/MainWindow/ResultPage/EditMailDetailsModal.jsx
@@ -107,7 +107,7 @@ const EditMailDetailsModal = ({
         <button
           type="submit"
           form="edit-mail-details-form"
-          className={`primary modal-button`}
+          className={`btn-primary modal-button`}
           disabled={isSubmitDisabled}
         >
           Send Mail
