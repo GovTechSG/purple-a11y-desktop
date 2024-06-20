60bea65b5dc2891533cd46f810249cd8f3c5afc3
angyonghaseyo <chiaangyong@gmail.com>
2024-06-20 11:21:17 +0800

    feat/localFileScan: Add support for local file scan, but pending on select file icon and logic to support that

--- src/MainWindow/HomePage/index.jsx
+++ src/MainWindow/HomePage/index.jsx
@@ -8,7 +8,13 @@ import labModeOn from "../../assets/lab-icon-on.svg";
 import InitScanForm from "./InitScanForm";
 import "./HomePage.scss";
 import services from "../../services";
-import { cliErrorCodes, cliErrorTypes, errorStates, versionComparator, urlWithoutAuth } from "../../common/constants";
+import {
+  cliErrorCodes,
+  cliErrorTypes,
+  errorStates,
+  versionComparator,
+  urlWithoutAuth,
+} from "../../common/constants";
 import Modal from "../../common/components/Modal";
 import { BasicAuthForm, BasicAuthFormFooter } from "./BasicAuthForm";
 import EditUserDetailsModal from "./EditUserDetailsModal";
@@ -19,9 +25,9 @@ import AboutModal from "./AboutModal";
 
 const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
   const navigate = useNavigate();
-  const [prevUrlErrorMessage, setPrevUrlErrorMessage] = useState('');
+  const [prevUrlErrorMessage, setPrevUrlErrorMessage] = useState("");
   const [{ name, email, browser, isLabMode }, setUserData] = useState({
-    name: "", 
+    name: "",
     email: "",
     browser: null,
     isLabMode: false,
@@ -31,7 +37,7 @@ const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
   const [showNoChromeErrorModal, setShowNoChromeErrorModal] = useState(false);
   const [showWhatsNewModal, setShowWhatsNewModal] = useState(false);
   const [showAboutPhModal, setShowAboutPhModal] = useState(false);
-  const [url, setUrl] = useState('');
+  const [url, setUrl] = useState("");
   const [scanButtonIsClicked, setScanButtonIsClicked] = useState(false);
   const [isAbortingScan, setIsAbortingScan] = useState(false);
 
@@ -48,29 +54,28 @@ const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
   }, []);
 
   // function that determines whether version is a prerelease/stable build
-  const getVersionLabel = useCallback((version) => {
-    const {
-      latestVer,
-      latestVerForLab,
-      allReleaseTags,
-      allPreReleaseTags
-    } = appVersionInfo;
+  const getVersionLabel = useCallback(
+    (version) => {
+      const { latestVer, latestVerForLab, allReleaseTags, allPreReleaseTags } =
+        appVersionInfo;
 
-    try {
-      if (latestVer === version) return 'latest stable build';
-      if (latestVerForLab === version) return 'latest pre-release';
+      try {
+        if (latestVer === version) return "latest stable build";
+        if (latestVerForLab === version) return "latest pre-release";
 
-      if (allReleaseTags.includes(version)) {
-        return 'stable build';
-      } else if (allPreReleaseTags.includes(version)) {
-        return 'pre-release';
-      }
-    } catch (error) {
+        if (allReleaseTags.includes(version)) {
+          return "stable build";
+        } else if (allPreReleaseTags.includes(version)) {
+          return "pre-release";
+        }
+      } catch (error) {
         console.log("Unable to show version label");
-    }
-    
-    return  undefined; // if cannot be determined, this should not happen
-  }, [appVersionInfo]);
+      }
+
+      return undefined; // if cannot be determined, this should not happen
+    },
+    [appVersionInfo]
+  );
 
   const isLatest = () => {
     const currVer = appVersionInfo.appVersion;
@@ -98,9 +103,9 @@ const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
 
   useEffect(() => {
     if (scanButtonIsClicked && prevUrlErrorMessage) {
-      setPrevUrlErrorMessage('');
+      setPrevUrlErrorMessage("");
     }
-  }, [scanButtonIsClicked])
+  }, [scanButtonIsClicked]);
 
   useEffect(() => {
     if (
@@ -110,11 +115,12 @@ const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
       setShowBasicAuthModal(true);
     }
 
-    if (prevUrlErrorMessage !== null && 
-        prevUrlErrorMessage.includes("No chrome browser")
+    if (
+      prevUrlErrorMessage !== null &&
+      prevUrlErrorMessage.includes("No chrome browser")
     ) {
       setShowNoChromeErrorModal(true);
-    } 
+    }
   }, [prevUrlErrorMessage]);
 
   useEffect(() => {
@@ -125,11 +131,11 @@ const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
       const handleShowModal = () => {
         setShowWhatsNewModal(!!userData["firstLaunchOnUpdate"] && isLatest());
         window.services.editUserData({ firstLaunchOnUpdate: false });
-      }
+      };
       const whatsNewModalTimeout = setTimeout(
         handleShowModal,
-        !!userData["firstLaunchOnUpdate"] ? 500 : 0,
-      )
+        !!userData["firstLaunchOnUpdate"] ? 500 : 0
+      );
       return whatsNewModalTimeout;
     };
 
@@ -138,20 +144,20 @@ const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
   }, []);
 
   const editUserData = (info) => {
-    setUserData(initData => ({ ...initData, ...info }));
+    setUserData((initData) => ({ ...initData, ...info }));
     window.services.editUserData(info);
   };
 
   useEffect(() => {
     const checkChromeExists = async () => {
       const chromeExists = await window.services.checkChromeExistsOnMac();
-      
+
       if (!chromeExists) {
         setShowNoChromeErrorModal(true);
       }
-    }
+    };
     checkChromeExists();
-  }, [])
+  }, []);
 
   const isValidHttpUrl = (input) => {
     const regexForUrl = new RegExp("^(http|https):/{2}.+$", "gmi");
@@ -159,9 +165,9 @@ const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
   };
 
   const isValidFilepath = (input) => {
-    const regexForFilepath = new RegExp("^(file:\/\/).+$", "gmi");
+    const regexForFilepath = new RegExp("^(file://).+$", "gmi");
     return regexForFilepath.test(input);
-  }
+  };
 
   const startScan = async (scanDetails) => {
     scanDetails.browser = isProxy ? "edge" : browser;
@@ -174,10 +180,19 @@ const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
     }
 
     if (scanDetails.scanType === "Sitemap crawl") {
-      if (!isValidHttpUrl(scanDetails.scanUrl) && !isValidFilepath(scanDetails.scanUrl)) {
+      if (
+        !isValidHttpUrl(scanDetails.scanUrl) &&
+        !isValidFilepath(scanDetails.scanUrl)
+      ) {
         setScanButtonIsClicked(false);
         setPrevUrlErrorMessage("Invalid sitemap.");
-        return; 
+        return;
+      }
+    } else if (scanDetails.scanType === "Local file") {
+      if (!isValidFilepath(scanDetails.scanUrl)) {
+        setScanButtonIsClicked(false);
+        setPrevUrlErrorMessage("Invalid FilePath. Please type in file:/// format.");
+        return;
       }
     } else if (!isValidHttpUrl(scanDetails.scanUrl)) {
       setScanButtonIsClicked(false);
@@ -193,38 +208,44 @@ const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
 
     window.localStorage.setItem("scanDetails", JSON.stringify(scanDetails));
 
-    const checkUrlResponse = await services.validateUrlConnectivity(scanDetails);
+    const checkUrlResponse = await services.validateUrlConnectivity(
+      scanDetails
+    );
     if (checkUrlResponse.success) {
-          navigate('/scanning', { state: { url: urlWithoutAuth(scanDetails.scanUrl).toString() } });
-          const scanResponse = await services.startScan(scanDetails);
+      navigate("/scanning", {
+        state: { url: urlWithoutAuth(scanDetails.scanUrl).toString() },
+      });
+      const scanResponse = await services.startScan(scanDetails);
 
-          if (scanResponse.cancelled){
-            return;
-          }
+      if (scanResponse.cancelled) {
+        return;
+      }
 
-          if (scanResponse.failedToCreateExportDir) {
-            setPrevUrlErrorMessage('Unable to create download directory');
-            return;
-          }
+      if (scanResponse.failedToCreateExportDir) {
+        setPrevUrlErrorMessage("Unable to create download directory");
+        return;
+      }
 
-          if (scanResponse.success) {
-            setCompletedScanId(scanResponse.scanId);
-            if (scanDetails.scanType === 'Custom flow') {
-              navigate('/custom_flow', { state: { scanDetails }})
-            } else {
-            navigate("/result");
-            }
-            return;
-          } else {
-            /* When no pages were scanned (e.g. out of domain upon redirects when valid URL was entered),
+      if (scanResponse.success) {
+        setCompletedScanId(scanResponse.scanId);
+        if (scanDetails.scanType === "Custom flow") {
+          navigate("/custom_flow", { state: { scanDetails } });
+        } else {
+          navigate("/result");
+        }
+        return;
+      } else {
+        /* When no pages were scanned (e.g. out of domain upon redirects when valid URL was entered),
                 redirects user to error page to going to result page with empty result */
-            navigate("/error", { state: { errorState: errorStates.noPagesScannedError,timeOfScan }});
-            return;
-          }
+        navigate("/error", {
+          state: { errorState: errorStates.noPagesScannedError, timeOfScan },
+        });
+        return;
+      }
     } else {
       setScanButtonIsClicked(false);
       if (checkUrlResponse.failedToCreateExportDir) {
-        setPrevUrlErrorMessage('Unable to create download directory');
+        setPrevUrlErrorMessage("Unable to create download directory");
         return;
       }
 
@@ -245,11 +266,14 @@ const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
             errorMessageToShow = "Invalid sitemap.";
             break;
           case cliErrorTypes.browserError:
-            navigate('/error', { state: { errorState: errorStates.browserError,timeOfScan }});
+            navigate("/error", {
+              state: { errorState: errorStates.browserError, timeOfScan },
+            });
             return;
           case cliErrorTypes.systemError:
           default:
-            errorMessageToShow = "Something went wrong. Please try again later.";
+            errorMessageToShow =
+              "Something went wrong. Please try again later.";
         }
         console.log(`status error: ${checkUrlResponse.statusCode}`);
         setPrevUrlErrorMessage(errorMessageToShow);
@@ -341,18 +365,21 @@ const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
           />
         </>
       )}
-      {showNoChromeErrorModal &&
-        <NoChromeErrorModal showModal={showNoChromeErrorModal} setShowModal={setShowNoChromeErrorModal}/>            
-      }
-      {showWhatsNewModal && getReleaseNotesOnUpdate(appVersionInfo) && 
+      {showNoChromeErrorModal && (
+        <NoChromeErrorModal
+          showModal={showNoChromeErrorModal}
+          setShowModal={setShowNoChromeErrorModal}
+        />
+      )}
+      {showWhatsNewModal && getReleaseNotesOnUpdate(appVersionInfo) && (
         <WhatsNewModal
           showModal={showWhatsNewModal}
           setShowModal={setShowWhatsNewModal}
           version={appVersionInfo.appVersion}
           releaseNotes={getReleaseNotesOnUpdate(appVersionInfo)}
         />
-      }
-      {showAboutPhModal &&
+      )}
+      {showAboutPhModal && (
         <AboutModal
           showModal={showAboutPhModal}
           setShowModal={setShowAboutPhModal}
@@ -361,7 +388,7 @@ const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
           isLabMode={isLabMode}
           setIsLabMode={(bool) => editUserData({ isLabMode: bool })}
         />
-      }
+      )}
       <div id="home-page-footer">
         <img
           id="app-illustration"
@@ -376,10 +403,16 @@ const HomePage = ({ isProxy, appVersionInfo, setCompletedScanId }) => {
                 className="purple-text"
                 onClick={() => setShowAboutPhModal(true)}
               >
-                <img className="me-2" src={isLabMode ? labModeOn : labModeOff} alt="" />
+                <img
+                  className="me-2"
+                  src={isLabMode ? labModeOn : labModeOff}
+                  alt=""
+                />
                 Version {appVersionInfo.appVersion}{" "}
-                {getVersionLabel(appVersionInfo.appVersion) && `(${getVersionLabel(appVersionInfo.appVersion)})`}
-              </Button> | Built by GovTech Accessibility Enabling Team
+                {getVersionLabel(appVersionInfo.appVersion) &&
+                  `(${getVersionLabel(appVersionInfo.appVersion)})`}
+              </Button>{" "}
+              | Built by GovTech Accessibility Enabling Team
             </>
           }
         </span>
