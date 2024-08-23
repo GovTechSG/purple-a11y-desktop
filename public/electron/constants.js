ffaa796555e4a8c92ef6ab910daf1b4a34f25118
younglim <younglim@users.noreply.github.com>
2024-08-23 09:54:51 +0800

    Revert pre-packed backend filename

--- public/electron/constants.js
+++ public/electron/constants.js
@@ -45,8 +45,7 @@ const getMacOSExecutablePath = () => {
 };
 const macOSExecutablePath = getMacOSExecutablePath();
 
-// const macOSPrepackageBackend = path.join(macOSExecutablePath, "Contents", "Resources", "oobee-portable-mac.zip");
-const macOSPrepackageBackend = path.join(process.resourcesPath, "oobee-portable-mac.zip");
+const macOSPrepackageBackend = path.join(process.resourcesPath, "purple-a11y-portable-mac.zip");
 
 const resultsPath =
   os.platform() === "win32"
