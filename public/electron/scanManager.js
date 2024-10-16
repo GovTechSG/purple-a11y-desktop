274569325e49d99f8eab07e02ff8e2a757ecd6c9
younglim <younglim@users.noreply.github.com>
2024-10-16 16:03:56 +0800

    Support large heap of up to 6GB

--- public/electron/scanManager.js
+++ public/electron/scanManager.js
@@ -133,7 +133,7 @@ const validateUrlConnectivity = async (scanDetails) => {
   const response = await new Promise(async (resolve) => {
     const check = spawn(
       "node",
-      [`${enginePath}/dist/cli.js`, ...getScanOptions(scanDetails)],
+      [`--max-old-space-size=6144`,`${enginePath}/dist/cli.js`, ...getScanOptions(scanDetails)],
       {
         cwd: resultsPath,
         env: {
